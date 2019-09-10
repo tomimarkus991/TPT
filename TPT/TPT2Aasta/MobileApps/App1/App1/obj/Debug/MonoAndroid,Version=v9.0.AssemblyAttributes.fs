ab7bd8d191646b270147bc708c5f7575af0d9a49
tomimarkus991 <tomimarkusalber@gmail.com>
2019-09-10 19:38:57 +0300

    Update

--- TPT/TPT2Aasta/MobileApps/App1/App1/obj/Debug/MonoAndroid,Version=v9.0.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute("MonoAndroid,Version=v9.0", FrameworkDisplayName="Xamarin.Android v9.0 Support")>]
-                do ()
---
+++ TPT/TPT2Aasta/MobileApps/HelloWorld/HelloWorld/SecondActivity.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloWorld
+{
+    [Activity(Label = "SecondActivity")]
+    class SecondActivity : Activity
+    {
+        EditText _arvField1;
+        EditText _arvField2;
+
+        Button _liitmine;
+        Button _lahutamine;
+        Button _korrutamine;
+        Button _jagamine;
+
+        TextView _vastus;
+
+        protected override void OnCreate(Bundle savedInstanceState) // Main function
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.layout_second);
+
+            // Application
+
+            _arvField1 = FindViewById<EditText>(Resource.Id.arv1);
+            _arvField2 = FindViewById<EditText>(Resource.Id.arv2);
+
+            _liitmine = FindViewById<Button>(Resource.Id.liitmine);
+            _lahutamine = FindViewById<Button>(Resource.Id.lahutamine);
+            _korrutamine = FindViewById<Button>(Resource.Id.korrutamine);
+            _jagamine = FindViewById<Button>(Resource.Id.jagamine);
+
+            _vastus = FindViewById<TextView>(Resource.Id.vastus);
+
+           _liitmine.Click += delegate
+           {
+               try
+               {
+                   double a = Convert.ToDouble(_arvField1.Text);            
+                   double b = Convert.ToDouble(_arvField2.Text);
+                   double c = a + b;
+                   _vastus.Text = Convert.ToString(c);
+               }
+               catch(System.FormatException)
+               {
+                   _vastus.Text = "You can enter only numbers";
+               }
+           };
+
+           _lahutamine.Click += delegate
+           {
+               try
+               {
+                   double a = Convert.ToDouble(_arvField1.Text); // see on string ja ss convertin intiks            
+                   double b = Convert.ToDouble(_arvField2.Text); // ja pärast kuvan selle _arvField1.Text
+                   double c = a - b;
+                   _vastus.Text = Convert.ToString(c);
+               }
+               catch(System.FormatException)
+               {
+                   _vastus.Text = "You can enter only numbers";
+               }              
+           };
+
+           _korrutamine.Click += delegate
+           {
+               try
+               {
+                   double a = Convert.ToDouble(_arvField1.Text);
+                   double b = Convert.ToDouble(_arvField2.Text);
+                   double c = a * b;
+                   _vastus.Text = Convert.ToString(c);
+               }
+               catch(System.FormatException)
+               {
+                   _vastus.Text = "You can enter only numbers";
+               }
+           };
+
+           _jagamine.Click += delegate
+           {
+               try
+               {
+                   double a = Convert.ToDouble(_arvField1.Text);
+                   double b = Convert.ToDouble(_arvField2.Text);
+                   double c = Math.Round(a / b, 2);
+                   _vastus.Text = Convert.ToString(c);
+               }
+               catch(System.FormatException)
+               {
+                   _vastus.Text = "You can enter only numbers";
+               }
+               catch(System.NullReferenceException)
+               {
+                   _vastus.Text = "You can't divide by 0";
+               }
+           };
+        }
+    }
+}
