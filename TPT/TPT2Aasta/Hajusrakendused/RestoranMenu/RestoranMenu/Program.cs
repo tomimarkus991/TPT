7a833e34ad52ce7519969c224d472d4be59f9847
Tomi Markus Alber <tomimarkus.alber@tptlive.ee>
2019-10-23 15:45:00 +0300

    Added xml file, updated TODORakendus

---
+++ TPT/TPT2Aasta/Hajusrakendused/RestoranMenu/RestoranMenu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RestoranMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\TPT\TPT\TPT2Aasta\Hajusrakendused\XML_Kodut66\Menu.xml");
+            XElement contactBook = XElement.Load(sr);
+            Console.WriteLine(contactBook);
+            Console.ReadKey();
+        }
+    }
+}
--- TPT/TPT2Aasta/Hajusrakendused/TODORakendus/toDoRakendus.js
+++ TPT/TPT2Aasta/Hajusrakendused/TODORakendus/toDoRakendus.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+
 window.addEventListener("DOMContentLoaded", () =>
 {
     fetch("http://demo2.z-bit.ee/todo.json")
@@ -56,3 +57,74 @@ function deleteRow(event)
 {
     event.currentTarget.parentElement.parentElement.remove();
 }
+function parseJSON(response)
+{
+    console.log(response);
+    return response.json();
+}
+function login()
+{
+    let username =  document.querySelector("input[name = 'username']").value;
+    let password =  document.querySelector("input[name = 'password']").value;
+
+    api.sendRequest("/users/get-token", "POST", {username, password}, (user) => {console.log(user); api.token = user.access_token;
+    api.sendRequest("/tasks", "GET", null, (tasks) => {
+        clearTodoList();
+        [tasks].forEach((task) => {
+            addToDo(task);
+            });
+        });
+     });
+
+}
+
+function register()
+{
+    let username =  document.querySelector("input[name = 'username']").value;
+    let password =  document.querySelector("input[name = 'password']").value;
+
+    api.sendRequest("/users", "POST", {username, newPassword: password}, (data) => {console.log(data);
+    });
+}
+function clearTodoList()
+{
+    const list = document.querySelector("#todo-list");
+    [...list.children].forEach((child) =>
+    {
+        if(!child.classList.contains("hidden"))
+            child.remove();
+    })
+}
+class Demo2Api
+{
+    constructor()
+    {
+        this.baseUrl = "http://demo2.z-bit.ee"
+    }
+    sendRequest(url, method, body, callback)
+    {
+        let username =  document.querySelector("input[name = 'username']").value;
+        let password =  document.querySelector("input[name = 'password']").value;
+
+        const options =
+        {
+            method,
+            headers:
+            {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        };
+
+        if(method == "GET" || method == "OPTION") delete options.body;
+
+        if(this.token !== null)
+        {
+            options.headers["Authorization"] = "Bearer " + this.token;
+        }
+        fetch(this.baseUrl + url, options)
+        .then((response) => response.json())
+        .then(callback);
+    }
+}
+const api = new Demo2Api();
