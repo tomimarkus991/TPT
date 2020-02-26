5d84b479c128048f05cddd367fb73c73f62fa75e
tomimarkus991 <tomimarkusalber@gmail.com>
2020-02-26 20:50:57 +0200

    Updated readme.md and fixed react submit button

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # TPT
-Here are my projects that I made in TPT (Tallinn Polytechnic School)
+Here is a proof that I did something in TPT (Tallinn Polytechnic School)
 
-My main programming language is C#. I also use JavaScript and I'm taking an Udemy Course for HTML and CSS.
+My main programming language is C#. I also use JavaScript.
 I have used Nightwatch.js for End-to-End testing.
+I am learning React with Tailwind.
+I have made several apps with Xamarin Forms and Xamarin Android
 
 My buddies from school and I, are working on a School Project named
 [Workaflow](https://github.com/tomimarkus991/Workaflow-1)
--- TPT/TPT_SecondYear/React/my-app/src/AddOperator.js
+++ TPT/TPT_SecondYear/React/my-app/src/AddOperator.js
@@ -19,14 +19,32 @@ class AddOperator extends Component {
       return (
          <div className='AddOp'>
             <form
-               className='bg-white-900 shadow-md rounded px-8 pt-6 pb-8 mb-4'
+               className='bg-white-900 shadow-md rounded px-64 pt-6 pb-8 mb-10'
                onSubmit={this.handleSubmit}>
-               <input className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type='text' id='name' placeholder="Name" onChange={this.handleChange} />
+               <input
+                  className='mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-auto'
+                  type='text'
+                  id='name'
+                  placeholder='Name'
+                  onChange={this.handleChange}
+               />
                <br />
-               <input className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type='text' id='name' placeholder="Age" onChange={this.handleChange} />
+               <input
+                  className='mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  type='text'
+                  id='age'
+                  placeholder='Age'
+                  onChange={this.handleChange}
+               />
                <br />
-               <input className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type='text' id='name' placeholder="Belt" onChange={this.handleChange} />
-               <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+               <input
+                  className='mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  type='text'
+                  id='belt'
+                  placeholder='Belt'
+                  onChange={this.handleChange}
+               />
+               <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
                   Submit
                </button>
             </form>
