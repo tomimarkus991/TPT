3cabba1e2de49196675f3fd3cc03852cdcac17fa
tomimarkus991 <tomimarkusalber@gmail.com>
2020-03-05 19:13:33 +0200

    Add my boilerplate project make it work like pokemon web app. reloading about and contact component dont work

---
+++ TPT/TPT_SecondYear/React/Poke-Times/src/app/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+   return (
+      <div>
+         <h2>Homeghfffffffffffffffffffffff</h2>
+         <p>See on meie homepahe</p>
+      </div>
+   );
+};
+
+export default Home;
