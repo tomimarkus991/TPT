faac8e8c79f7a0f0ba91d2a236d7915df7c111e6
tomimarkus991 <tomimarkusalber@gmail.com>
2020-03-22 12:15:54 +0200

    Added 2 buttons to add and decrease a number

--- TPT/TPT_SecondYear/React/PokeTimes2/src/app/components/Contact.js
+++ TPT/TPT_SecondYear/React/PokeTimes2/src/app/components/Contact.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Contact = (props) => {
-  // setTimeout(() => {
-  //    props.history.push("/about");
-  // }, 2000);
-  return (
-    <div className="text-center">
-      <h1>Contact</h1>
-      <p>Kontakteeruge minguga meili teel</p>
-    </div>
-  );
-};
+class Contact extends Component {
+  state = {
+    number: 0,
+  };
+  handleAdd = () => {
+    const count = this.state.number;
+    this.setState({
+      number: count + 1,
+    });
+  };
+  handleDecrease = () => {
+    const count = this.state.number;
+    this.setState({
+      number: count - 1,
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <button
+          className="bg-red-600 border-black border-2 text-white p-4"
+          onClick={this.handleAdd}>
+          Add
+        </button>
+        <button
+          className="bg-red-600 border-black border-2 text-white p-4"
+          onClick={this.handleDecrease}>
+          Substract
+        </button>
+        <h1>{this.state.number}</h1>
+      </div>
+    );
+  }
+}
 
 export default Contact;
